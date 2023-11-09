005f6fcaca5f4d05f7ad0c9764d8f57c36293cf1
Halina2610 <vievamaxa@gmail.com>
2023-11-09 16:53:25 +0300

    exams 8 - 13

---
+++ src/exam13/13_03.tsx
@@ -0,0 +1,37 @@
+export const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'USER-NAME-UPDATED':
+           // return XXX
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    name: action.name //верно
+                }
+            }
+
+        default:
+            return state
+    }
+}
+
+const updateUserNameAC = (name: string) => ({type: 'USER-NAME-UPDATED', name})
+
+
+const state = {
+    count: 10,
+    user: {
+        name: 'Dimych',
+        age: 18,
+        isMarried: true,
+        status: "offline"
+    },
+    books: ['you don\'t know JS']
+}
+const newState = reducer(state, updateUserNameAC('Dmitry'))
+
+console.log(newState.user.name === 'Dmitry')
+console.log(newState.books === state.books)
+console.log(newState.user !== state.user)
+
+//Что нужно написать вместо XXX, чтобы корректно обновить имя пользователя и в консоли увидеть:  true true true?
