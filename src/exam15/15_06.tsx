289f8120387e4f1d3adefe9568e20ed68677559b
Halina2610 <vievamaxa@gmail.com>
2023-12-25 15:03:44 +0300

    exams 17-18

---
+++ src/exam15/15_06.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+
+const thunkCreator = () => (XXX: any, YYY: any) => {
+    // сode...
+}
+
+
+// App
+const App = () => {
+    return (
+        <>
+            <h1>В этом задании смотреть на экран не нужно. Ничего не изменится 😈</h1>
+            <p>Читайте описание к заданию</p>
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App/>)
+
+// 📜 Описание:
+// Вместо XXX и YYY через пробел напишите параметры которые приходят в санку.
+//
+// 🖥 Пример ответа: useCallback state ответ: dispatch getState
